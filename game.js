56ba8529deb6ddbbb29bbc7688bdfefb3eca931b
spacebattle-web <spacebattledemo1@hl3shop.com>
2024-09-05 22:37:22 -0400

    test

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const player = {
     speed: 10,
     image: new Image()
 };
-player.image.src = 'assets/player.png';
+player.image.src = 'https://raw.githubusercontent.com/microsoft/Web-Dev-For-Beginners/main/6-space-game/6-end-condition/your-work/assets/player.png';
 
 let enemies = [];
 let lasers = [];
@@ -176,7 +176,7 @@ function shootLaser() {
         speed: 10,
         image: new Image()
     };
-    laser.image.src = 'assets/laserRed.png';
+    laser.image.src = 'https://raw.githubusercontent.com/microsoft/Web-Dev-For-Beginners/main/6-space-game/6-end-condition/your-work/assets/laserRed.png';
     lasers.push(laser);
 }
 
@@ -189,7 +189,7 @@ function spawnEnemies() {
             height: 50,
             image: new Image()
         };
-        enemy.image.src = '/assets/enemyShip.png';
+        enemy.image.src = 'https://raw.githubusercontent.com/microsoft/Web-Dev-For-Beginners/main/6-space-game/6-end-condition/your-work/assets/enemyShip.png';
         enemies.push(enemy);
     }
 }
